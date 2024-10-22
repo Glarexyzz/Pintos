6e4a10637218e1d55c78ec21a4367aa62e5a55bc
jx1823 <jx1823@ic.ac.uk>
2024-10-22 01:59:51 +0100

    fix: added initiation of original_priority for a thread

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -527,6 +527,9 @@ init_thread (struct thread *t, const char *name, int priority)
   t->priority = priority;
   t->magic = THREAD_MAGIC;
 
+  t->original_priority = priority;
+
+
   old_level = intr_disable ();
   list_push_back (&all_list, &t->allelem);
   intr_set_level (old_level);
