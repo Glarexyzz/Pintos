d8894609f66d0ac3bd8fc7d70635ebe4b17dbe05
jx1823 <jx1823@ic.ac.uk>
2024-11-11 11:29:40 +0000

    fix: access the physical address of the file by user input, check for null return

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -214,11 +214,24 @@ static void wait(struct intr_frame *f) {
  */
 static void create(struct intr_frame *f) {
   // bool create(const char *file, unsigned initial_size)
-  const char *file = ARG(const char *, 1);
+  const char *user_filename = ARG(const char *, 1);
   unsigned initial_size = ARG(unsigned , 2);
 
+  //Access memory
+  const char *physical_file_address = access_user_memory(
+      thread_current()->pagedir,
+      user_filename
+  );
+
+  // Terminating the offending process and freeing its resources
+  // for invalid pointer address.
+  if (physical_file_address == NULL) {
+    exit_process(-1);
+    NOT_REACHED();
+  }
+
   lock_acquire(&file_system_lock);
-  bool success = filesys_create(file, initial_size);
+  bool success = filesys_create(physical_file_address, initial_size);
   lock_release(&file_system_lock);
 
   f->eax = success;
