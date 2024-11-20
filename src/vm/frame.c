090bd33b079335cb9833ed1a29a89126e654186a
Alexander Biraben-Renard <ab1223@ic.ac.uk>
2024-11-20 13:47:06 +0000

    feat: define structs and global variables for frame table
    Co-authored-by: Jacky Xiao <jx1823@ic.ac.uk>
    Co-authored-by: Mohammad A <ma3523@ic.ac.uk>

--- src/vm/frame.c
+++ src/vm/frame.c
@@ -1,1 +1,14 @@
 #include "vm/frame.h"
+#include "threads/synch.h"
+#include "threads/thread.h"
+
+/// The item to be inserted into the frame struct's owners list
+struct owner {
+  struct thread *process; /* The thread/process which owns a page */
+  struct list_elem elem;  /* For insertion into the frame's owner list */
+};
+
+/// The frame table
+struct hash frame_table;
+/// The lock for the frame table
+struct lock frame_table_lock;
--- src/vm/frame.h
+++ src/vm/frame.h
@@ -1,4 +1,19 @@
 #ifndef VM_FRAME_H
 #define VM_FRAME_H
 
+#include <hash.h>
+#include <list.h>
+
+/// Item to insert into the frame table
+struct frame {
+  void *kvaddr;                /* Kernel virtual address */
+  bool ref_bit;                /* Reference bit for eviction */
+  struct list owners;          /* List of processes which own the frame */
+  struct list_elem queue_elem; /* For insertion into eviction queue */
+  struct hash_elem table_elem; /* For insertion into frame table */
+};
+
+struct hash frame_table;
+struct lock frame_table_lock;
+
 #endif /* vm/frame.h */
